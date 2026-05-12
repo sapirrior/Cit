bd9b645dbf2b030fb415def074d727780c9fcc5b
Nolan Stark <246715373+sapirrior@users.noreply.github.com>
2026-05-12 07:14:11 +0530

    bump: version to 0.13.0-beta

--- src/main.c
+++ src/main.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[]) {
     }
 
     if (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--version") == 0) {
-        printf("Cit version %s\n", CIT_VERSION);
+        printf("%s\n", CIT_VERSION);
         return 0;
     }
 
--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #ifndef CIT_VERSION_H
 #define CIT_VERSION_H
 
-#define CIT_VERSION "0.12.1-beta"
+#define CIT_VERSION "0.13.0-beta"
 
 #endif /* CIT_VERSION_H */
